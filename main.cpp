321241f040023de3caed4b30fb562708848fca32
Aurèle Oulès <aurele@oules.com>
2017-05-26 12:13:13 +0200

    Tail follows snake's head, kill itself when head goes into tail

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ bool gameOver;
 const int width = 40;
 const int height = 20;
 int x,y, fruitsX, fruitsY, score;
+int tailX[100], tailY[100];
+int nTail = 0;
 enum eDirection {STOP=0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -69,7 +71,16 @@ void Draw() {
             } else if(i == fruitsY && j == fruitsX) {
                 cout << "F";
             } else {
-                cout << " ";
+                bool print = false;
+                for(int k = 0; k < nTail; k++) {
+                    if(tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if(!print) {
+                    cout << " ";
+                }
             }
             if(j == width - 1) {
                 cout << "#";
@@ -81,6 +92,7 @@ void Draw() {
         cout << "#";
     }
     cout << endl;
+    cout << "Score: " << score << endl;
 }
 
 void Input() {
@@ -103,9 +115,37 @@ void Input() {
                 break;
         }
     }
+    if(x > width || x < 0 || y > height || y < 0) {
+        gameOver = true;
+    }
+    for(int i = 0; i < nTail; i++) {
+        if(tailX[i] == x && tailY[i] == y) {
+            gameOver = true;
+        }
+    }
+    if(x == fruitsX && y == fruitsY) {
+        score+= 10;
+        fruitsX = rand() % width;
+        fruitsY = rand() % height;
+        nTail++;
+    }
 }
 
 void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
     switch(dir) {
         case LEFT:
             x--;
